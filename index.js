c898fa3fe6ad9ec52af42fad287f3ada0885acd6
Sait Sumeli <95253541+saitsumeli@users.noreply.github.com>
2023-03-27 13:51:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import axios from 'axios'
 
 // axios await async kullanarak API üzerinden verileri çekmek;
 
-(async() => {
-    const {data : user1} = await axios("https://jsonplaceholder.typicode.com/users/1"); 
-    console.log(user1)
 
-    const {data : posts} = await axios("https://jsonplaceholder.typicode.com/posts/1")
-    console.log("posts:" , posts)
-})();
+const {data: res} = await axios (
+`https://jsonplaceholder.typicode.com/users/${Number}`
+)
+ const {data: res1} = await axios (
+        `https://jsonplaceholder.typicode.com/posts?id=${Number}`
+)
+console.log("res",res);
+console.log("res1",res1);
+}
+
